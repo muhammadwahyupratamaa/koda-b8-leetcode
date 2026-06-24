c574608d928f9d1fa6322062196b1dacb8072c92
Muhammad Wahyu Pratama <muhammad123wahyu123@gmail.com>
2026-06-24 14:15:03 +0700

    answer 14. Longest Common Prefix

--- 3.js
+++ 3.js
@@ -1,3 +1,48 @@
-const str = "Life, the universe and everything. Answer:";
+/**
+ * 14. Longest Common Prefix
+ * 
+ * 
+ * Write a function to find the longest common prefix string amongst an array of strings.
 
-console.log(`${str} ${str.length}`);
+If there is no common prefix, return an empty string "".
+
+ 
+
+Example 1:
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+Example 2:
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+ 
+
+Constraints:
+
+1 <= strs.length <= 200
+0 <= strs[i].length <= 200
+strs[i] consists of only lowercase English letters if it is non-empty.
+ */
+
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    let prefix = strs[0];
+
+    for (let i = 1 ; i < strs.length ; i++) {
+        while (!strs[i].startsWith(prefix)) {
+            prefix= prefix.slice(0,-1)
+        }
+        if(prefix === "") {
+            return ""
+        }
+    }
+    return prefix
+};
+
+console.log(longestCommonPrefix(["flower","flow","flight"]))
+console.log(longestCommonPrefix(["dog","racecar","car"]))
